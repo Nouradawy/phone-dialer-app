8709a04594f67215b0d715847ccd3819cd5528f8
Nouradawy <nouradawyonex@gmail.com>
2021-11-08 01:34:02 +0200

    Making the app Null-Safty + phone Blocking now working -needs to work on permisions - Adding Endcall and Answercall functionality

---
+++ assets/packages/flutter_phone_state/example/android/app/src/main/kotlin/co/sunnyapp/flutter_phone_state_example/MainActivity.kt
@@ -0,0 +1,9 @@
+package co.sunnyapp.flutter_phone_state_example
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugins.firebase.core.FirebaseCorePlugin;
+
+class MainActivity: FlutterActivity() {
+   
+}
